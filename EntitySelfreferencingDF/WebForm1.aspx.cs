2d93f489dbc46a4c3560776df7f3c617454bff49
Marian Marian <fenrir55@gmail.com>
2018-12-09 12:13:15 +0100

    wersja 1

---
+++ EntitySelfreferencingDF/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EntitySelfreferencingDF
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            EmployeeDBContext db = new EmployeeDBContext();
+            GridView1.DataSource = db.Employees5.Select(emp => new { EmployeeName = emp.EmployeeName, MamagerName = emp.Manager.EmployeeName == null ? "Super Boss" :emp.Manager.EmployeeName }).ToList();
+            GridView1.DataBind();
+        }
+    }
+}
